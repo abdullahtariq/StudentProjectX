595f81a02531e6377a8417e71cee4d9b1958ced9
abdullahtariq1 <faabdullahtariq@gmail.com>
2020-10-02 15:23:50 +0500

    upadting records

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello Express','subtemplate' : 'students' });
+  res.render('index', { title: 'Student Portal','subtemplate' : 'students' });
 });
 
 router.get('/signup',function(req,res,next){
